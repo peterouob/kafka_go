df3cf48adc0ede4efa6f295452733b727781be67
林庭楷 <hellolinpeter@gmail.com>
2024-05-11 22:14:55 +0800

    feat:consumer

--- consumer/main.go
+++ consumer/main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 
 	"github.com/IBM/sarama"
+	"github.com/spf13/viper"
 )
 
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigFile("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	servers := []string{"localhost:9092"}
-	consumer, err := sarama.NewConsumer(servers, nil)
+	consumer, err := sarama.NewConsumerGroup(viper.GetStringSlice("kafka.servers"), viper.GetString("kafka.group"), nil)
 	if err != nil {
 		panic(err)
 	}
 	defer consumer.Close()
-	partition, err := consumer.ConsumePartition("bondhi", 0, sarama.OffsetNewest)
-	if err != nil {
-		panic(err)
-	}
-	defer partition.Close()
-
-	fmt.Println("Consumer start.")
-	for {
-		select {
-		case err := <-partition.Errors():
-			fmt.Println(err)
-		case msg := <-partition.Messages():
-			println(string(msg.Value))
-		}
-	}
+	consumer.Consume()
 }
+
+// func main() {
+// 	servers := []string{"localhost:9092"}
+// 	consumer, err := sarama.NewConsumer(servers, nil)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer consumer.Close()
+// 	partition, err := consumer.ConsumePartition("bondhi", 0, sarama.OffsetNewest)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer partition.Close()
+
+// 	fmt.Println("Consumer start.")
+// 	for {
+// 		select {
+// 		case err := <-partition.Errors():
+// 			fmt.Println(err)
+// 		case msg := <-partition.Messages():
+// 			println(string(msg.Value))
+// 		}
+// 	}
+// }
